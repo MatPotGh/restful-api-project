6d1228d4c726c357ff6c987c6937029431d350c4
Mateusz <empsi15@gmail.com>
2023-06-30 16:35:06 +0200

    Cofigure database and create schema with initial data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,29 @@
+INSERT INTO user_info(login, request_counter) 
+VALUES ('visionmedia',2);
+
+INSERT INTO user_info(login, request_counter) 
+VALUES ('c9s',4);
+
+INSERT INTO user_info(login, request_counter) 
+VALUES ('fabpot',6);
+
+INSERT INTO user_info(login, request_counter) 
+VALUES ('weierophinney',8);
+
+INSERT INTO user_info(login, request_counter) 
+VALUES ('springmeyer',10);
+
+INSERT INTO user_info(login, request_counter) 
+VALUES ('dcramer',12);
+
+INSERT INTO user_info(login, request_counter) 
+VALUES ('jeromeetienne',14);
+
+INSERT INTO user_info(login, request_counter) 
+VALUES ('ornicar',16);
+
+INSERT INTO user_info(login, request_counter) 
+VALUES ('davglass',18);
+
+INSERT INTO user_info(login, request_counter) 
+VALUES ('postmodern',20);
