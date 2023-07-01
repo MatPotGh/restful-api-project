da4591d6ef328f655407ba8587ec754cf485e119
Mateusz <empsi15@gmail.com>
2023-07-01 22:13:00 +0200

    Improve logging and change calculation result decimal precision

--- src/main/java/com/empik/restfulapiproject/service/UserInfoService.java
+++ src/main/java/com/empik/restfulapiproject/service/UserInfoService.java
@@ -1,5 +1,11 @@
 package com.empik.restfulapiproject.service;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+
+import org.springframework.stereotype.Service;
+
 import com.empik.restfulapiproject.exception.RestApiException;
 import com.empik.restfulapiproject.mapper.UserInfoMapper;
 import com.empik.restfulapiproject.model.entity.UserInfo;
@@ -7,44 +13,38 @@ import com.empik.restfulapiproject.model.response.GithubUserInfoResponse;
 import com.empik.restfulapiproject.model.response.UserInfoResponse;
 import com.empik.restfulapiproject.model.type.ExceptionCodeType;
 import com.empik.restfulapiproject.repository.UserInfoRepository;
+
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 
 @Slf4j
 @Service
 @AllArgsConstructor
 public class UserInfoService {
 
-    private final GithubApiClient githubApiClient;
-    private final UserInfoRepository userInfoRepository;
-    private final UserInfoMapper userInfoMapper;
+	private final GithubApiClient githubApiClient;
+	private final UserInfoRepository userInfoRepository;
+	private final UserInfoMapper userInfoMapper;
 
-    public UserInfoResponse getUserInfoByGithubLogin(String login) {
-        log.info("[getUserInfoByGithubLogin] invoked with login=[{}]", login);
-        GithubUserInfoResponse githubUserInfo = githubApiClient.getUserInfoByGithubLogin(login);
-        UserInfo userInfo = userInfoRepository.findByLogin(githubUserInfo.getLogin())
-                .orElse(userInfoMapper.toEntity(githubUserInfo.getLogin()));
+	public UserInfoResponse getUserInfoByGithubLogin(String login) {
+		GithubUserInfoResponse githubUserInfo = githubApiClient.getUserInfoByGithubLogin(login);
+		UserInfo userInfo = userInfoRepository.findByLogin(githubUserInfo.getLogin())
+				.orElse(userInfoMapper.toEntity(githubUserInfo.getLogin()));
+		userInfo.incrementRequestCounter();
+		BigDecimal calculations = calculate(githubUserInfo);
+		userInfoRepository.save(userInfo);
+		log.info("[toResponse] invoked with githubUserInfo=[{}] and calculations=[{}]", githubUserInfo,calculations);
+		return userInfoMapper.toResponse(githubUserInfo, calculations);
+	}
 
-        userInfo.incrementRequestCounter();
-        BigDecimal calculations = calculate(githubUserInfo);
-
-        userInfoRepository.save(userInfo);
-        return userInfoMapper.toResponse(githubUserInfo, calculations);
-    }
-
-    private BigDecimal calculate(GithubUserInfoResponse githubUserInfo) {
-        log.info("[calculate] invoked with githubUserInfo=[{}]", githubUserInfo);
-        Long numberOfFollowers = githubUserInfo.getNumberOfFollowers();
-        if (numberOfFollowers == null || numberOfFollowers == 0) {
-            log.error("[calculate] Followers number for the user is equal to 0. Calculations can't be proceeded.");
-            throw new RestApiException(ExceptionCodeType.ILLEGAL_CALCULATION_ARGUMENT);
-        }
-        return BigDecimal.valueOf(6.0)
-                .divide(BigDecimal.valueOf(numberOfFollowers),3,RoundingMode.HALF_UP)
-                .multiply(BigDecimal.valueOf(2.0).add(BigDecimal.valueOf(githubUserInfo.getNumberOfPublicRepositories())));
-    }
+	private BigDecimal calculate(GithubUserInfoResponse githubUserInfo) {
+		log.info("[calculate] invoked with githubUserInfo=[{}]", githubUserInfo);
+		Long numberOfFollowers = githubUserInfo.getNumberOfFollowers();
+		if (numberOfFollowers == null || numberOfFollowers == 0) {
+			log.error("[calculate] Followers number for the user is equal to 0. Calculations can't be proceeded.");
+			throw new RestApiException(ExceptionCodeType.ILLEGAL_CALCULATION_ARGUMENT);
+		}
+		return BigDecimal.valueOf(6.0).divide(BigDecimal.valueOf(numberOfFollowers), 12, RoundingMode.HALF_UP).multiply(
+				BigDecimal.valueOf(2.0).add(BigDecimal.valueOf(githubUserInfo.getNumberOfPublicRepositories()))).stripTrailingZeros();
+	}
 }
